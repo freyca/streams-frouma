7358891e75db58ae9c50889b138b8771caa79fb2
Fran Rey <franreycastedo@gmail.com>
2025-09-30 20:45:56 +0200

    add: css laravel style

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -7,11 +7,6 @@
     <title>Frouma stream</title>
     <link rel="icon" href="/images/favicon.png" type="image/png" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link href="https://vjs.zencdn.net/8.3.0/video-js.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="/css/form.css" rel="stylesheet">
-    <link href="/css/chat.css" rel="stylesheet">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
